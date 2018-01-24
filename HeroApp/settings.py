a9062c1fa91adf7439ea5d0428ab55de8937c53b
Nima karimi <nima.karimi@live.com>
2018-01-24 16:40:04 +0330

    'bastappinhtm3l'

--- HeroApp/settings.py
+++ HeroApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ry0!63x=om9s=2=jyuui_zpk40qly0=_opxt-_w)#pthao40%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
