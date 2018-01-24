43243893c3d606db0f2895363b76de50875dc53b
Nima karimi <nima.karimi@live.com>
2018-01-24 15:55:29 +0330

    Main Page

--- thugh/views.py
+++ thugh/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
+
+class HomePageView(TemplateView):
+    template_name = 'thugh/index.html'
 # Create your views here.
